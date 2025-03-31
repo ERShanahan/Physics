c565f218703736e847cb987870c3445c60efb9e5
ERShanahan <eshanaha@stevens.edu>
2025-03-31 19:32:14 -0400

    Bunch of Stuffs

--- particles/defs.h
+++ particles/defs.h
@@ -5,14 +5,14 @@
 #define WINDOW_Y 800
 
 #define GRAVITY 1000.0f
-#define ENTROPY 0.01f
+#define ENTROPY 0.5f
 
-#define NUM_PARTICLES 1000
-#define RADIUS 5.0f
+#define NUM_PARTICLES 20000
+#define RADIUS 0.5f
 #define DIMENSION 2
 #define CELL_SIZE 10
 
 #define MOUSE_RADIUS 100.0f
-#define MOUSE_FORCE 100000.0f
+#define MOUSE_FORCE 1000.0f
 
 #endif // DEFS_H
